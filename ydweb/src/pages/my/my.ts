cce746ee4d78094d03d16e6b4caef86d3c14c6a7
Sunhuizhe <674277449@qq.com>
2018-04-27 10:54:01 +0800

    add my page

--- ydweb/src/pages/my/my.ts
+++ ydweb/src/pages/my/my.ts
@@ -22,4 +22,16 @@ export class MyPage {
     console.log('ionViewDidLoad MyPage');
   }
 
+  userinfo={
+    name:'-留白。',
+    intro:'要有最朴素的生活和遥远的梦想，即使明天天寒地冻，路遥马亡。——海子'.slice(0,16)+'...'
+  }
+
+  items=[
+    {iconname:'md-pulse',title:'我的运动'},
+    {iconname:'md-star',title:'我的收藏'},
+    {iconname:'ios-call',title:'我要合作'},
+    {iconname:'md-information-circle',title:'关于悦动'}
+  ]
+
 }
