48f47dfe460c525d711f6c09a6c0328129e1254c
hh970304 <1575994297@qq.com>
2018-04-27 10:16:29 +0800

    register

--- ydweb/src/app/app.component.ts
+++ ydweb/src/app/app.component.ts
@@ -4,12 +4,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
+import { RegisterPage } from '../pages/register/register';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage:any = RegisterPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
