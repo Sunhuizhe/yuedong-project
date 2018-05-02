a672cb0035e742bde7d4cab1f60fe3e0d154a375
Sunhuizhe <674277449@qq.com>
2018-05-02 17:16:06 +0800

    add about page

--- ydweb/src/pages/about/about.ts
+++ ydweb/src/pages/about/about.ts
@@ -7,8 +7,36 @@ import { NavController } from 'ionic-angular';
 })
 export class AboutPage {
 
+  actype:string='order';
+
   constructor(public navCtrl: NavController) {
 
   }
 
+  // 场地预约
+  addressitems=[
+    {imgs:'assets/imgs/order1.jpg',name:'河北师范大学轮滑场',grade:'4.7',orders:'25',position:'河北省石家庄市南二环东路20号',price:'10'},
+    {imgs:'assets/imgs/order1.jpg',name:'河北师范大学轮滑场',grade:'4.7',orders:'25',position:'河北省石家庄市南二环东路20号',price:'20'},
+    {imgs:'assets/imgs/order1.jpg',name:'河北师范大学轮滑场',grade:'4.7',orders:'25',position:'河北省石家庄市南二环东路20号',price:'30'},
+    {imgs:'assets/imgs/order1.jpg',name:'河北师范大学轮滑场',grade:'4.7',orders:'25',position:'河北省石家庄市南二环东路20号',price:'40'},
+    {imgs:'assets/imgs/order1.jpg',name:'河北师范大学轮滑场',grade:'4.7',orders:'25',position:'河北省石家庄市南二环东路20号',price:'50'}
+  ]
+
+  // 发起活动
+  myactitems=[
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'},
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'},
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'},
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'},
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'}
+  ]
+ 
+  // 参加活动
+  actitems=[
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'},
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'},
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'},
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'},
+    {imgs:'assets/imgs/launch1.jpg',name:'荧光夜跑',position:'河北师范大学风雨操场',stime:'5月3日19:00',payway:'AA',number:'不限',etime:'6月3日21:00'}
+  ]
 }
