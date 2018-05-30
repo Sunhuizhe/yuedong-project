970b0369c7a6362d8ac012f04cb81392802276e1
Sunhuizhe <674277449@qq.com>
2018-05-30 15:25:17 +0800

    修改场地预约页

--- ydweb/src/pages/about-site/about-site.ts
+++ ydweb/src/pages/about-site/about-site.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { Geolocation } from '@ionic-native/geolocation';
 /**
  * Generated class for the AboutSitePage page.
  *
@@ -15,11 +15,31 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AboutSitePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+// 信息列表
+  infoItems={
+    name:'河北师范大学-白馆',
+    grade:4.7,
+    time:'8:00-21:00',
+    tel:'15703286327',
+    place:'石家庄市裕华区南二环东路20号，河北师范大学新校区',
+    intro:'室内场地,活动请穿软底运动鞋,爱护场地设施，严禁在场内吸烟、吃东西，乱扔废弃物,场地服务人员为学生志愿者，进场人员要服从志愿者的管理和安排'
+  };
+
+  constructor(private geolocation: Geolocation,
+    public navCtrl: NavController, 
+    public navParams: NavParams) {
+     
+  }
+
+  // 跳转申请页
+  goOrder(){
+    this.navCtrl.push('AboutOrderPage');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AboutSitePage');
   }
+  
+
 
 }
