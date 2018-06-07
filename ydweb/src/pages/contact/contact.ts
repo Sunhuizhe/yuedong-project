d0bb33d73bca4c4894edffd49249a139c6ca2ac3
Sunhuizhe <674277449@qq.com>
2018-06-07 14:55:57 +0800

    修改发现页面

--- ydweb/src/pages/contact/contact.ts
+++ ydweb/src/pages/contact/contact.ts
@@ -7,8 +7,87 @@ import { NavController } from 'ionic-angular';
 })
 export class ContactPage {
 
+  items = [
+    {
+      name: '如魔似佛像我',
+      time: '今天 09：08',
+      content: '我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！',
+      image: 'assets/imgs/QQ1.jpg',
+      imgslength:9,
+      imgs: [
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+        { url: 'assets/imgs/login.jpg' },
+      ]
+    },
+    {
+      name: '如魔似佛像我',
+      time: '今天 09：08',
+      content: '我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！',
+      image: 'assets/imgs/QQ.jpg',
+      imgslength:1,
+      imgs: [
+        { url: 'assets/imgs/login.jpg' },
+      ]
+    },
+    {
+      name: '如魔似佛像我',
+      time: '今天 09：08',
+      content: '我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！',
+      image: 'assets/imgs/QQ.jpg',
+      imgslength:4,
+      imgs: [
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+      ]
+    },
+    {
+      name: '如魔似佛像我',
+      time: '今天 09：08',
+      content: '我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！',
+      image: 'assets/imgs/QQ1.jpg',
+      imgslength:6,
+      imgs: [
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+      ] // imgs
+    }, // item
+    {
+      name: '如魔似佛像我',
+      time: '今天 09：08',
+      content: '我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！我想带你去游山玩水，看那落霞与孤鹜齐飞！',
+      image: 'assets/imgs/QQ1.jpg',
+      imgslength:2,
+      imgs: [
+        { url: 'assets/imgs/login.jpg' },
+        { url: 'assets/imgs/login.png' },
+      ] // imgs
+    } // item
+  ] // items
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  goContactUP(){
+    this.navCtrl.push('ContactUpPage');
+  }
+
+
+  
+
+
 
 }
