72e38202f35852410bfa9225674a55532a65f7a6
Sunhuizhe <674277449@qq.com>
2018-06-20 17:20:30 +0800

    分类活动页添加其他分类

--- ydweb/src/pages/home-act-class/home-act-class.ts
+++ ydweb/src/pages/home-act-class/home-act-class.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HTTP } from '@ionic-native/http';
+import { AlertController } from 'ionic-angular';
 
 /**
  * Generated class for the HomeActClassPage page.
@@ -16,14 +17,28 @@ import { HTTP } from '@ionic-native/http';
 })
 export class HomeActClassPage {
 
-  actClass:'';
-  myactitems;
+  actClass: '';
+  myactitems = [];
+  otherAct = [];
 
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
     private http: HTTP,
+    private alertCtrl: AlertController,
     public navParams: NavParams) {
+
     this.actClass = navParams.data;
-    console.log(this.actClass);
+  }
+
+  // 下拉刷新
+  doRefresh(refresher) {
+    console.log('下拉刷新-热门运动-begin', refresher);
+
+    this.request();
+
+    setTimeout(() => {
+      console.log('下拉刷新-热门运动-ended');
+      refresher.complete();
+    }, 2000);
   }
 
   ionViewDidLoad() {
@@ -32,27 +47,71 @@ export class HomeActClassPage {
 
   ionViewWillEnter() {
     console.log('ionViewWillEnter AboutPage');
-
-      // 活动请求
-      this.http.post('http://39.107.66.152:8080/sport/getActList',{
-        userID:'all'
-      },{}).then(res=>{
-        // console.log(res['data']);
-        var data = JSON.parse(res['data']);
-
-        for(var i in data){
-          this.myactitems[i] = data[i];
-          console.log(data[i]['actTime']);
-          var actTime = new Date(data[i]['actTime']);
-          this.myactitems[i]['actTime'] = actTime.toLocaleString();
-          // console.log(date2);
-          var actCutOffTime = new Date(data[i]['actCutOffTime']);
-          this.myactitems[i]['actCutOffTime'] = actCutOffTime.toLocaleString();
-        }
-        // console.log(this.myactitems);
-      }).catch(err=>{
-        console.log(err);
-      })
-
+    this.request();
   }
+
+  // 错误信息提示框
+  presentAlert(mes) {
+    let alert = this.alertCtrl.create({
+      // title: 'Low battery',
+      subTitle: mes,
+      buttons: ['知道了！']
+    });
+    alert.present();
+  }
+
+  // 请求
+  request() {
+    // 活动请求
+    this.http.post('http://39.107.66.152:8080/goodActInfo', {
+      className: this.actClass
+    }, {}).then(res => {
+      console.log(res['data']);
+      var data = JSON.parse(res['data']);
+      // this.myactitems = data;
+      for (var i in data) {
+
+        // console.log(data[i]['actTime']);
+        var actTime = new Date(data[i]['actTime']);
+        data[i]['actTime'] = actTime.toLocaleString();
+        // this.myactitems[i]['actTime'] = actTime.toLocaleString();
+        // console.log(date2);
+        var actCutOffTime = new Date(data[i]['actCutOffTime']);
+        data[i]['actCutOffTime'] = actCutOffTime.toLocaleString();
+        // this.myactitems[i]['actCutOffTime'] = actCutOffTime.toLocaleString();
+
+        console.log('类别：', data[i].actClass);
+
+        if (data[i].actClass == '其他') {
+          console.log(data[i].actClass == '其他', i);
+          this.otherAct.push(data[i]);
+        } else {
+          console.log(data[i].actClass == '其他', i);
+          this.myactitems.push(data[i]);
+        }
+      }
+      if (this.myactitems.length != 0) {
+        var empty = document.getElementById('empty');
+        empty.style.display = 'none';
+      }
+      // console.log(this.myactitems);
+    }).catch(err => {
+      console.log('err:', err);
+      this.presentAlert(err);
+    });
+
+    for (var k in this.myactitems) {
+      console.log(this.myactitems[k]);
+    }
+  }
+
+  // 跳转添加活动页
+  goAboutUp() {
+    this.navCtrl.push('AboutUpPage');
+  }
+
+  goAct(item) {
+    this.navCtrl.push('AboutActPage', item);
+  }
+
 }
