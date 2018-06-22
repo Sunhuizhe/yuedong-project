6cfa60ada0604821b34b76070b48fbaf14661cbe
Sunhuizhe <674277449@qq.com>
2018-06-22 08:20:16 +0800

    添加圈子发图功能

--- ydweb/src/pages/contact-up/contact-up.ts
+++ ydweb/src/pages/contact-up/contact-up.ts
@@ -22,13 +22,14 @@ export class ContactUpPage {
     time: '',
     content: '',
     image: '',
-    imgslength:2,
+    imgslength: 2,
     imgs: []
-  } 
+  }
+  base64Image: any;
 
   constructor(public navCtrl: NavController,
     private camera: Camera,
-    public actionSheetCtrl: ActionSheetController,    
+    public actionSheetCtrl: ActionSheetController,
     public navParams: NavParams) {
   }
 
@@ -36,114 +37,107 @@ export class ContactUpPage {
     console.log('ionViewDidLoad ContactUpPage');
   }
 
+  // 图片上传
+  pictureLoad() {
+
+    let actionSheet = this.actionSheetCtrl.create({
+      title: '选择图片',
+      buttons: [
+        {
+          text: '从相册',
+          role: 'destructive',
+          handler: () => {
+            console.log('从相册');
+            this.changePicture();
+          }
+        },
+        {
+          text: '拍照',
+          handler: () => {
+            console.log('拍照');
+            this.changeCamera();
+          }
+        },
+        {
+          text: '取消',
+          role: 'cancel',
+          handler: () => {
+            console.log('取消上传');
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
+
   // 从相册上传
-  changePicture(e) {
-    console.log('1');
+  changePicture() {
+
     const options: CameraOptions = {
-      sourceType:2,
+      sourceType: 2,
       quality: 100,
       destinationType: 0,
       encodingType: 0,
       mediaType: 0,
-      allowEdit:true,
-      targetWidth:300,
-      targetHeight:300,
-      correctOrientation:true
+      allowEdit: true,
+      targetWidth: 300,
+      targetHeight: 300,
+      correctOrientation: true
     }
 
-    let base64Image,base64;
     this.camera.getPicture(options).then((imageData) => {
-      base64Image = 'data:image/jpeg;base64,' + imageData;
-      e.target.setAttribute('src', base64Image);      
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      var node = document.getElementById('addImage');
+      var node2 = document.getElementById('image');
+      node.style.display = 'none';
+      node2.style.display = 'block';
+      node2.style.background = 'url(' + this.base64Image + ')';
+
     }, (err) => {
       // Handle error
-      console.log('err1:',err);
+      console.log('err1:', err);
     });
-
-
   }
 
   // 从相机上传
-  changeCamera(e) {
-    console.log(e.target.getAttribute('src'));
+  changeCamera() {
+
     const options: CameraOptions = {
       quality: 100,
       destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE,
-      allowEdit:true,
-      // targetWidth:300,
-      // targetHeight:300,
+      allowEdit: true,
+      targetWidth: 300,
+      targetHeight: 300,
     }
 
-    let base64Image;
-
     this.camera.getPicture(options).then((imageData) => {
-      base64Image = 'data:image/jpeg;base64,' + imageData;
-      e.target.setAttribute('src', base64Image);
-
-      // this.http.post('http://39.107.66.152:8080/mine/avatar',{},{
-      //   'Content-Type':'application/x-www-form-urlencoded'
-      // }).then(res=>{
-      //   console.log('res:',res);
-      // },err=>{
-      //   console.log('err:',err);
-      //   for(var k in err){
-      //     console.log(k,err[k]);
-      //   }
-      // });
-
-      // console.log(base64Image);
+      this.base64Image = 'data:image/jpeg;base64,' + imageData;
+      var node = document.getElementById('addImage');
+      var node2 = document.getElementById('image');
+      node.style.display = 'none';
+      node2.style.display = 'block';
+      node2.style.background = 'url(' + this.base64Image + ')';
     }, (err) => {
       // Handle error
       console.log(err);
     });
 
-    
+
   }
 
-  // 图片上传
-  upLoadPicture() {
-    // let actionSheet = this.actionSheetCtrl.create({
-    //   title: '选择图片',
-    //   buttons: [
-    //     {
-    //       text: '从相册',
-    //       role: 'destructive',
-    //       handler: () => {
-    //         console.log('从相册');
-    //         this.changePicture(e);
-    //       }
-    //     },
-    //     {
-    //       text: '拍照',
-    //       handler: () => {
-    //         console.log('拍照');
-    //         this.changeCamera(e);
-    //       }
-    //     },
-    //     {
-    //       text: '取消',
-    //       role: 'cancel',
-    //       handler: () => {
-    //         console.log('取消上传');
-    //       }
-    //     }
-    //   ]
-    // });
-
-    // actionSheet.present();
-  }
 
   // 数据获取 
-  setValue(e){
+  setValue(e) {
     console.log(e.target.value);
     this.userinfo.content = e.target.value;
   }
 
   // 请求数据
-  request(){
-    
+  request() {
+
   }
 
 }
